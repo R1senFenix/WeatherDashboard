71157a1ddfdb4495ff83cd836afed6394eef10b0
RisenFenix <jjstram@gmail.com>
2020-01-31 23:02:08 -0600

    init commit and basic ajax calling... having trouble with var being undef though i have defined them

---
+++ HomeworkInstructions/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    if (typeof localSearches === 'undefined') {
+        var recentSearches = [];
+    } else {
+        var recentSearches = JSON.parse(localSearches.getItem("recentSearches"));
+        console.log(recentSearches);
+    }
+    // get the search input
+    $('.submit-btn').click(function () {
+        event.preventDefault();
+        var searchForCity = $('.search-for-city').val();
+        console.log(searchForCity);
+        var searchForCountry = $('.search-for-country').val();
+        var searchForBoth = searchForCity + "," + searchForCountry;
+        searchWeather(searchForBoth);
+        console.log(searchForBoth);
+        saveLocal(searchForCity);
+    });
+
+    //Calls the ajax
+    function searchWeather(searchForBoth) {
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + searchForBoth + '&APPID=9281dbe33d6371c2c8b80f96b4c64d8b&mode=json',
+            method: "GET",
+            id: 2643741,
+            dataType: "json",
+        }).then(function (response) {
+            console.log(response);
+        });
+    }
+
+    function saveLocal() {
+        recentSearches.push(searchForCity);
+        localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+        //addToDrop();
+    }
+});
